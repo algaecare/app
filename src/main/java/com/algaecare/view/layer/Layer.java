aa06344cdc71cac2246974d660a58c425622c87d
Tim Buser <tim.buser@youengineering.ch>
2025-05-03 17:04:43 +0200

    update abstract Layer type

--- src/main/java/com/algaecare/view/layer/Layer.java
+++ src/main/java/com/algaecare/view/layer/Layer.java
@@ -11,10 +11,8 @@ public abstract class Layer extends StackPane {
 
     public Layer(LayerType type) {
         this.layerType = type;
-        setHeight(Double.MAX_VALUE);
-        setWidth(Double.MAX_VALUE);
-        setMaxWidth(Double.MAX_VALUE);
-        setMaxHeight(Double.MAX_VALUE);
+        setPickOnBounds(false);
+        setAlignment(javafx.geometry.Pos.TOP_LEFT);
     }
 
     public LayerType getLayerType() {
