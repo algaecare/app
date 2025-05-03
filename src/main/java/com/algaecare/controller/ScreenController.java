513cdfdc61b02f53f5a1c1dd1c8ca892474f2334
Tim Buser <tim.buser@youengineering.ch>
2025-05-03 10:44:36 +0200

    update ScreenController to use new TextLayer

--- src/main/java/com/algaecare/controller/ScreenController.java
+++ src/main/java/com/algaecare/controller/ScreenController.java
@@ -1,21 +1,23 @@
 package com.algaecare.controller;
 
-import java.util.logging.Logger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.algaecare.model.GameState;
-import com.algaecare.view.*;
+import com.algaecare.model.TextLayerData;
+import com.algaecare.view.MainScene;
 import com.algaecare.view.layer.DynamicLayer;
 import com.algaecare.view.layer.Layer;
 import com.algaecare.view.layer.StaticLayer;
-import javafx.stage.Stage;
+import com.algaecare.view.layer.TextLayer;
+
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class ScreenController implements GameStateChangeListener {
     private static final Logger LOGGER = Logger.getLogger(ScreenController.class.getName());
-    // make a list of layers that are static or animated
     private final List<Layer> layers;
     private final MainScene scene;
 
@@ -33,18 +35,23 @@ public class ScreenController implements GameStateChangeListener {
         StaticLayer layerThree = new StaticLayer("/static/3-LAYER-3.png");
         StaticLayer layerTwo = new StaticLayer("/static/2-LAYER-2.png");
         StaticLayer layerOne = new StaticLayer("/static/1-LAYER-1.png");
-        // StaticLayer layerZero = new StaticLayer("/static/0-SHEEN.png");
 
         // Initialize dynamic layers
         DynamicLayer axolotl = new DynamicLayer("dynamic/axolotl");
 
+        TextLayer titleTextLayer = new TextLayer(1040, 220, 450, 150, TextLayerData.getText("TITLE"), "SUPERWATER_BIG");
+        TextLayer subtitleTextLayer = new TextLayer(380, 100, 800, 390, TextLayerData.getText("SUBTITLE"),
+                "SUPERWATER_SMALL");
+
+        // Initialize text layers from JSON
         layers.add(backdropLayer);
         layers.add(backgroundLayer);
         layers.add(layerThree);
         layers.add(layerTwo);
         layers.add(layerOne);
-        // layers.add(layerZero);
         layers.add(axolotl);
+        layers.add(titleTextLayer);
+        layers.add(subtitleTextLayer);
     }
 
     public void updateScreen(GameState newState) {
