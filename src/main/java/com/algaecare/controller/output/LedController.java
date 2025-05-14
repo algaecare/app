4c8bc7df40fefdbf5b026f70aca5b0a604d9772e
Adrian Sonderegger <adrian.sonderegger@students.fhnw.ch>
2025-05-14 13:40:22 +0200

    fix led

--- src/main/java/com/algaecare/controller/output/LedController.java
+++ src/main/java/com/algaecare/controller/output/LedController.java
@@ -10,16 +10,33 @@ public class LedController implements GameStateEventManager {
     private static final int PIN_LEDS_LEVER = 11;
     private Environment environment;
     private Context pi4j;
+    private com.pi4j.io.gpio.digital.DigitalOutput[] ledOutputs;
+    private com.pi4j.io.gpio.digital.DigitalOutput leverLed;
 
     public LedController(Environment environment, Context pi4j) {
         this.environment = environment;
         this.pi4j = pi4j;
-        updateLed(PIN_LEDS_LEVER, true);
+        leverLed = pi4j.digitalOutput().create(PIN_LEDS_LEVER);
+        updateLeverLed(true);
+        ledOutputs = new com.pi4j.io.gpio.digital.DigitalOutput[PIN_LEDS.length];
+        for (int i = 0; i < PIN_LEDS.length; i++) {
+            int ledPin = PIN_LEDS[i];
+            var led = pi4j.digitalOutput().create(ledPin);
+            ledOutputs[i] = led;
+            updateLed(i, false);
+        }
+
+        for (int i = 0; i < PIN_LEDS.length; i++) {
+            updateLed(i, true);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) { }
+            updateLed(i, false);
+        }
     }
 
     public void updateLed(int ledPin, boolean on) {
-        var led = pi4j.digitalOutput().create(ledPin);
-
+        com.pi4j.io.gpio.digital.DigitalOutput led = ledOutputs[ledPin];
         if (on) {
             led.high();
         } else {
@@ -27,17 +44,25 @@ public class LedController implements GameStateEventManager {
         }
     }
 
+    public void updateLeverLed(boolean on) {
+        if (on) {
+            leverLed.high();
+        } else {
+            leverLed.low();
+        }
+    }
+
     @Override
     public void onGameStateChanged(GameState oldState, GameState newState) {
         int level = environment.getAlgaeLevel();
 
-        int thresholdIndex = Math.max(0, Math.round((float) level / (float) 100 * (float) 6));
+        int thresholdIndex = ((level * 6) / 100);
 
         for (int i = 0; i < PIN_LEDS.length; i++) {
-            if (i <= thresholdIndex) {
-                updateLed(PIN_LEDS[i], true);
+            if (i >= thresholdIndex) {
+                updateLed(i, false);
             } else {
-                updateLed(PIN_LEDS[i], false);
+                updateLed(i, true);
             }
         }
     }
