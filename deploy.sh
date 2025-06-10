86fdb27e18579a9ca00e8896f2feec249b9c740f
Tim Buser <timbuser@proton.me>
2025-06-10 10:04:23 +0200

    remove deprecated sh scripts

--- deploy.sh
+++
@@ -1,1 +0,0 @@
-scp -r /Users/timbuser/Documents/PRIMEOENERGIE/algae-care.zip pi@100.107.22.123:/home/pi/algaecare/app
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xmx2g -XX:+UseG1GC -jar target/algae-care-1.0-SNAPSHOT.jar
